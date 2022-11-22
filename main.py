808d1a10754acf4da43842d7d91b12b10f78f2e6
Nick Markels <markels23@up.edu>
2022-11-22 11:07:29 -0800

    Imported music21 library and loaded song in

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("Test")
-print("hello world/")
+from music21 import converter,instrument # or import *
+file = converter.parse('ACDC.Highway_to_Hell_K.mid')
+components = []
+for element in file.recurse():
+    components.append(element)
+
+print(components)
