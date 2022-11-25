5fb1342532a995d7fb050565cafa3c912f2034bf
Nick Markels <markels23@up.edu>
2022-11-25 11:02:16 -0800

    Convert to 32 bit float
    main.py: checks if features.txt exists, if not loads features from wav file
    and creates features.txt
    
    music_loader.py: Converts all feature vectors to 32 bit floats to help shorten
    file length

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import feature_loader as fl
 import clustering as cl
+import music_loader as ml
 
 # Get the train and test data
+#Checks if feature text file exists,
+#if not creates file from wav
+
+if(ml.is_loaded() == False):
+    ml.load_wav()
+   
 train, test = fl.getFeatures()
 print(train.shape)
+    
+
 
 # Run k-means
 cl.kmeans(train, test)
+
+print("Done")
