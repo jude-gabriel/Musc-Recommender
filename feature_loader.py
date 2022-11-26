1a69a2302b6b3a87436de64f52b6cb2798e563c9
Jack Volonte <volonte22@up.edu>
2022-11-25 21:57:24 -0800

    getFeatures() update
    added train and test split data to be returned from the getFeatures() function

--- feature_loader.py
+++ feature_loader.py
@@ -1,10 +1,14 @@
+import time
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn import decomposition
-import time
+from sklearn.preprocessing import StandardScaler
 
 # run a PCA and return the model
+
+
+
 def runPca(data, n_components):
     pca = PCA(n_components=n_components)
     principleComponents = pca.fit_transform(data)
@@ -23,7 +27,7 @@ def runPca(data, n_components):
 
     return  pca_data
 
-# write pca to file
+# computes pca and writes to file, returns train and test split datasets
 def writeToFile(pca_data, fileNames):
     fileWrite = open("featuresAfterPCA.txt", "w")
     count = 0
@@ -38,8 +42,21 @@ def getFeatures(n_components):
     features = np.loadtxt("features.txt", delimiter=",", dtype='float', comments='G',usecols=[0,497])
     fileNames = np.loadtxt("features.txt", delimiter=",",dtype='str', comments='00000000000', usecols=[498])
 
+    pca = runPca(features, n_components)
+    writeToFile(pca, fileNames) # runs and writes the PCA to file 'featuresAfterPCA.txt'
 
-    writeToFile(runPca(features, n_components), fileNames) # runs and writes the PCA to file 'featuresAfterPCA.txt'
+    train_music, test_music = train_test_split(features, test_size=1/7.0, random_state=0)
+
+
+    return(train_music, test_music)
+
+    # standardizing the datas features
+    scaler = StandardScaler()
+    scaler.fit(train_music)
+    train_music = scaler.transform(train_music)
+    test_music = scaler.transform(test_music)
+
+    # return(train_music, test_music) # return for post standard scale train and test sets
 
 
 # function call, runs pca on dataset in features.txt file, n_compenents size
