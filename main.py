dbb76b80fa407768f9610f0330d3363ec87cf188
Nick Markels <markels23@up.edu>
2022-11-27 22:27:41 -0800

    Changes

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import clustering as cl
 # Get the train and test data
 train, test = fl.getFeatures()
 
-
 # Run k-means
 songs_to_recommend = cl.kmeans(train, test)
 print(songs_to_recommend)
 
+#ok
